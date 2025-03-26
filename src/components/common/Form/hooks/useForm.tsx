9fe4b1c1a8892ef963428e168e9f048baba34500
deeharvey06 <cliffharvey06@gmail.com>
2025-03-26 14:21:00 -0700

    unpkgPathPlugin

--- src/components/common/Form/hooks/useForm.tsx
+++ src/components/common/Form/hooks/useForm.tsx
@@ -1,8 +1,12 @@
 import { useState, useCallback, ChangeEvent, MouseEvent } from 'react'
 
+import useEsbuild from '../../../../hooks/useEsbuild'
+import { unpkgPathPlugin } from '../../../../config/esbuild/plugins/unpkg-path-plugin'
+
 const useForm = () => {
   const [input, setInput] = useState<string>('')
-  const [code, setCode] = useState<string>('')
+  const [code, setCode] = useState('')
+  const { esbuild } = useEsbuild()
 
   const handleChange = useCallback((e: ChangeEvent<HTMLTextAreaElement>) => {
     const { value } = e.target
@@ -11,12 +15,26 @@ const useForm = () => {
   }, [])
 
   const handleClick = useCallback(
-    (e: MouseEvent<HTMLButtonElement>) => {
+    async (e: MouseEvent<HTMLButtonElement>) => {
       e.preventDefault()
 
-      setCode(input)
+      // const result = await esbuild?.transform(input, {
+      //   loader: 'jsx',
+      //   target: 'es2015',
+      // })
+
+      // setCode(result?.code || '')
+
+      const result = await esbuild?.build({
+        entryPoints: ['index.js'],
+        bundle: true,
+        write: false,
+        plugins: [unpkgPathPlugin()],
+      })
+
+      setCode(result?.outputFiles[0].text || '')
     },
-    [input]
+    [esbuild, input]
   )
 
   return { input, code, handleChange, handleClick }
